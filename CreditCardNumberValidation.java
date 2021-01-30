5486c3ad39aa1042745725bcde2f2b80dece3441
Onabanjo Titus <74909457+OnabanjoTitus@users.noreply.github.com>
2021-01-30 12:39:14 +0100

    new commit

--- CreditCardNumberValidation.java
+++ CreditCardNumberValidation.java
@@ -5,7 +5,10 @@ import java.math.BigInteger;
 
 public class CreditCardNumberValidation {
     private BigInteger creditCardNumber;
-
+    private boolean visaCard;
+    private boolean masterCard;
+    private boolean americanExpress;
+    private boolean discoverCards;
     public void setCreditCardNumber(String creditCardNumber) {
         this.creditCardNumber = new BigInteger( creditCardNumber);
     }
@@ -31,4 +34,48 @@ public class CreditCardNumberValidation {
         }
     return validity;
     }
+    public boolean IsVisaCard() {
+      checkCardType(creditCardNumber);
+        return visaCard;
+    }
+    public boolean IsMastercard() {
+        checkCardType(creditCardNumber);
+        return masterCard;
+    }
+    public boolean IsDiscovercard() {
+        checkCardType(creditCardNumber);
+        return discoverCards;
+    }
+    public boolean IsAmericanExpress() {
+        checkCardType(creditCardNumber);
+        return americanExpress;
+    }
+
+    private void checkCardType(BigInteger creditCardNumber) {
+
+        char number = caseCondition(creditCardNumber.toString());
+        switch (number){
+            case '4' ->
+                   visaCard = true;
+            case '5' ->
+                    masterCard = true;
+            case '6' ->
+                    discoverCards = true;
+            case '3' -> {
+                char output = americanExpressClause(creditCardNumber.toString());
+                        if(output == '7')
+                            americanExpress = true;
+            }
+        }
+
+    }
+    private char caseCondition(String input) {
+        return  input.charAt(0);
+    }
+    private char americanExpressClause(String input){
+        return input.charAt(1);
+
+    }
+
+
 }
